1b35e89bfe78991c7e62c6d1de56aa350ee29ecd
molanp <104612722+molanp@users.noreply.github.com>
2026-03-18 18:40:13 +0800

    refactor(constants): 移除httpx依赖并更新用户代理字符串
    移除httpx库依赖，因为不再需要其超时功能；更新浏览器标识字符串，
    使用Edge浏览器的最新版本标识替换旧的UC浏览器标识；删除常量文件中的超时配置定义
    
    BREAKING CHANGE: 移除了httpx依赖和相关超时配置

--- src/nonebot_plugin_parser_lite/constants.py
+++ src/nonebot_plugin_parser_lite/constants.py
@@ -1,12 +1,10 @@
 from enum import Enum
 from typing import Final
 
-from httpx import Timeout
-
 COMMON_HEADER: Final[dict[str, str]] = {
     "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/55.0.2883.87 UBrowser/6.2.4098.3 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/146.0.0.0 Safari/537.36 Edg/146.0.0.0"
     )
 }
 
@@ -23,12 +21,6 @@ ANDROID_HEADER: Final[dict[str, str]] = {
         "Chrome/132.0.0.0 Mobile Safari/537.36 Edg/132.0.0.0"
     )
 }
-
-COMMON_TIMEOUT: Final[Timeout] = Timeout(connect=15.0, read=20.0, write=10.0, pool=10.0)
-
-DOWNLOAD_TIMEOUT: Final[Timeout] = Timeout(
-    connect=15.0, read=240.0, write=10.0, pool=10.0
-)
 
 
 class PlatformEnum(str, Enum):
